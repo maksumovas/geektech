d7a8d9ddfcb10631a26d06d1c642a0c0906dfe67
maksumovas <118979305+maksumovas@users.noreply.github.com>
2022-12-10 20:30:17 +0600

    Update main.swift

--- dz/dz/dz7_1/dz7_1/main.swift
+++ dz/dz/dz7_1/dz7_1/main.swift
@@ -7,41 +7,31 @@
 
 var totalPrice: Double = 0
 var totalItems: Int = 0
+var totalQuantity: Int = 0
 
-func add(name: String, manufacturer: String, weight: Float, barcode: Int, price: Double, discount: String?, availability: String){
+func storeProducts(name: String, manufacturer: String, weight: Float, barcode: Int, price: Double, discount: String?, availability: String, quantity: Int){
     totalPrice += price
     totalItems += 1
+    totalQuantity += quantity
 }
 
 
-add(name:"Хлеб", manufacturer: "Хлебный дом", weight: 300, barcode: 4607177744216, price: 30, discount: nil, availability: "are available")
-add(name:"Молоко", manufacturer: "Веселый Молочник", weight: 900, barcode: 4607177744216, price: 125, discount: nil, availability: "are available")
-add(name:"Мясо говяжье", manufacturer: "Мираторг", weight: 1000, barcode: 4607177744216, price: 800, discount: nil, availability: "are available")
-add(name:"Курица", manufacturer: "Индилайт", weight: 1000, barcode: 4607177744216, price: 400, discount: nil, availability: "are available")
-add(name:"Масло растительное", manufacturer: "Слобода", weight: 1000, barcode: 4607177744216, price: 180, discount: nil, availability: "are available")
-add(name:"Масло сливочное", manufacturer: "Беловодское", weight: 500, barcode: 4607177744216, price: 300, discount: nil, availability: "are available")
-add(name:"Яйца куриные", manufacturer: "Птицеферма", weight: 550, barcode: 4607177744216, price: 120, discount: nil, availability: "are available")
-add(name:"Сливки", manufacturer: "Домик в деревне", weight: 480, barcode: 4607177744216, price: 105, discount: nil, availability: "are available")
-add(name:"Чипсы", manufacturer: "Lays", weight: 140, barcode: 4607177744216, price: 800, discount: nil, availability: "are available")
-add(name:"Семга", manufacturer: "Русское море", weight: 300, barcode: 4607177744216, price: 1500, discount: nil, availability: "are available")
-add(name:"Авокадо", manufacturer: "Бразилия", weight: 200, barcode: 4607177744216, price: 100, discount: nil, availability: "are available")
-add(name:"Бананы", manufacturer: "Индия", weight: 1000, barcode: 4607177744216, price: 155, discount: nil, availability: "are available")
-add(name:"Сахар", manufacturer: "Продимекс", weight: 1000, barcode: 4607177744216, price: 85, discount: nil, availability: "are available")
-add(name:"Вода", manufacturer: "Legend", weight: 500, barcode: 4607177744216, price: 25, discount: nil, availability: "are available")
-add(name:"Сметана", manufacturer: "Веселый Молочник", weight: 315, barcode: 4607177744216, price: 80, discount: nil, availability: "are available")
-add(name:"Апельсины", manufacturer: "Бразилия", weight: 1000, barcode: 4607177744216, price: 160, discount: nil, availability: "are available")
-add(name:"Виноград", manufacturer: "Узбекситан", weight: 1000, barcode: 4607177744216, price: 145, discount: nil, availability: "are available")
-add(name:"Грейпфрут", manufacturer: "Турция", weight: 1000, barcode: 4607177744216, price: 165, discount: nil, availability: "are available")
-add(name:"Яблоки зеленые", manufacturer: "Аргентина", weight: 1000, barcode: 4607177744216, price: 250, discount: nil, availability: "are available")
-add(name:"Огурцы", manufacturer: "Кыргызстан", weight: 1000, barcode: 4607177744216, price: 200, discount: nil, availability: "are available")
-add(name:"Брокколи", manufacturer: "Китай", weight: 400, barcode: 4607177744216, price: 190, discount: nil, availability: "are available")
-add(name:"Кола", manufacturer: "Coca Cola", weight: 1500, barcode: 4607177744216, price: 82, discount: nil, availability: "are available")
-add(name:"Сок", manufacturer: "J7", weight: 900, barcode: 4607177744216, price: 120, discount: nil, availability: "are available")
-add(name:"Холодный чай", manufacturer: "Fuse Tea", weight: 1000, barcode: 4607177744216, price: 74, discount: nil, availability: "are available")
-add(name:"Кофе растворимый", manufacturer: "Carte Noire", weight: 95, barcode: 4607177744216, price: 650, discount: nil, availability: "are available")
-add(name:"Мука", manufacturer: "Акун", weight: 2000, barcode: 4607177744216, price: 130, discount: nil, availability: "are available")
-add(name:"Соль", manufacturer: "4 life", weight: 500, barcode: 4607177744216, price: 100, discount: nil, availability: "are available")
-add(name:"Перец черный", manufacturer: "Royal", weight: 100, barcode: 4607177744216, price: 45, discount: nil, availability: "are available")
- 
+storeProducts(name:"Хлеб", manufacturer: "Хлебный дом", weight: 300, barcode: 4607177744216, price: 30, discount: nil, availability: "are available", quantity: 100)
+storeProducts(name:"Молоко", manufacturer: "Веселый Молочник", weight: 900, barcode: 4607177744216, price: 125, discount: nil, availability: "are available", quantity: 70)
+storeProducts(name:"Мясо говяжье", manufacturer: "Мираторг", weight: 1000, barcode: 4607177744216, price: 800, discount: nil, availability: "are available", quantity: 10)
+storeProducts(name:"Курица", manufacturer: "Индилайт", weight: 1000, barcode: 4607177744216, price: 400, discount: nil, availability: "are available", quantity: 5)
+storeProducts(name:"Масло растительное", manufacturer: "Слобода", weight: 1000, barcode: 4607177744216, price: 180, discount: nil, availability: "are available", quantity: 30)
+storeProducts(name:"Яйца куриные", manufacturer: "Птицеферма", weight: 550, barcode: 4607177744216, price: 120, discount: nil, availability: "are available", quantity: 30)
+storeProducts(name:"Сливки", manufacturer: "Домик в деревне", weight: 480, barcode: 4607177744216, price: 105, discount: nil, availability: "are available", quantity: 35)
+storeProducts(name:"Чипсы", manufacturer: "Lays", weight: 140, barcode: 4607177744216, price: 800, discount: nil, availability: "are available", quantity: 25)
+storeProducts(name:"Семга", manufacturer: "Русское море", weight: 300, barcode: 4607177744216, price: 1500, discount: nil, availability: "are available", quantity: 25)
+storeProducts(name:"Авокадо", manufacturer: "Бразилия", weight: 200, barcode: 4607177744216, price: 100, discount: nil, availability: "are available", quantity: 15)
+storeProducts(name:"Бананы", manufacturer: "Индия", weight: 1000, barcode: 4607177744216, price: 155, discount: nil, availability: "are available", quantity: 15)
+storeProducts(name:"Сахар", manufacturer: "Продимекс", weight: 1000, barcode: 4607177744216, price: 85, discount: nil, availability: "are available", quantity: 40)
+storeProducts(name:"Вода", manufacturer: "Legend", weight: 500, barcode: 4607177744216, price: 25, discount: nil, availability: "are available", quantity: 100)
+storeProducts(name:"Кола", manufacturer: "Coca Cola", weight: 1500, barcode: 4607177744216, price: 82, discount: nil, availability: "are available", quantity: 80)
+storeProducts(name:"Сок", manufacturer: "J7", weight: 900, barcode: 4607177744216, price: 120, discount: nil, availability: "are available", quantity: 70)
+
 print(totalPrice)
 print(totalItems)
+print(totalQuantity)
